e87cad0e3e8eeee4e0b115b68323a4988bf22689
Si Beaumont <simon.beaumont@citrix.com>
2014-10-17 15:36:39 +0100

    Test we can still make checks whe we call to another function
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- test/test_fail.ml
+++ test/test_fail.ml
@@ -1,2 +1,7 @@
 let rec length_nottailrec = function [] -> 0 | x::xs -> 1 + length_nottailrec xs
 [@@tailrec]
+
+(* Try using another function *)
+let one () = 1
+let rec length_nottailrec' = function [] -> 0 | x::xs -> one () + length_nottailrec' xs
+[@@tailrec]
